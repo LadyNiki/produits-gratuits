7389542d93816489f516e96494ab89510b9a5e66
LadyNiki <nikilouis31@gmail.com>
2026-07-20 14:33:38 -0400

    Correction du port pour Render

--- server/server.js
+++ server/server.js
@@ -116,7 +116,7 @@ app.delete("/api/produits/:id", (req, res) => {
 
 });
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Serveur lancé sur http://127.0.0.1:${PORT}`);

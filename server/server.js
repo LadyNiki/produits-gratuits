44e359cb7c94c8387c74199643a9c9611e3ef81f
LadyNiki <nikilouis31@gmail.com>
2026-07-21 23:15:15 -0400

    Correction du dossier uploads

--- server/server.js
+++ server/server.js
@@ -11,9 +11,14 @@ app.use(express.json());
 
 app.use(express.static(path.join(__dirname, "../public")));
 
+const uploadDir = path.join(__dirname, "../public/uploads");
+
+if (!fs.existsSync(uploadDir)) {
+  fs.mkdirSync(uploadDir, { recursive: true });
+}
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, path.join(__dirname, "../public/uploads"));
+  cb(null, uploadDir);
   },
   filename: (req, file, cb) => {
     cb(null, Date.now() + "-" + file.originalname);
